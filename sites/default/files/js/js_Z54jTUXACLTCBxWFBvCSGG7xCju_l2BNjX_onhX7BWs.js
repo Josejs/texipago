fdd23835ff56b6d80aaa83f34815659643ec2235
jose <cerberus.jos@gmail.com>
2013-11-21 05:12:44 -0600

    archivos comprimidos css

---
+++ sites/default/files/js/js_Z54jTUXACLTCBxWFBvCSGG7xCju_l2BNjX_onhX7BWs.js
@@ -0,0 +1,294 @@
+Drupal.locale = { 'pluralFormula': function ($n) { return Number(($n!=1)); }, 'strings': {"":{"An AJAX HTTP error occurred.":"Hubo un error HTTP AJAX.","HTTP Result Code: !status":"C\u00f3digo de Resultado HTTP: !status","An AJAX HTTP request terminated abnormally.":"Una solicitud HTTP de AJAX termin\u00f3 de manera anormal.","Debugging information follows.":"A continuaci\u00f3n se detalla la informaci\u00f3n de depuraci\u00f3n.","Path: !uri":"Ruta: !uri","StatusText: !statusText":"StatusText: !statusText","ResponseText: !responseText":"ResponseText: !responseText","ReadyState: !readyState":"ReadyState: !readyState","Next":"Siguiente","Edit":"Editar","Sunday":"Domingo","Monday":"Lunes","Tuesday":"Martes","Wednesday":"Mi\u00e9rcoles","Thursday":"Jueves","Friday":"Viernes","Saturday":"S\u00e1bado","Add":"Agregar","Configure":"Configurar","Done":"Hecho","Prev":"Previo","Mon":"Lun","Tue":"Mar","Wed":"Mi\u00e9","Thu":"Jue","Fri":"Vie","Sat":"S\u00e1b","Sun":"Dom","January":"Enero","February":"Febrero","March":"Marzo","April":"Abril","May":"Mayo","June":"Junio","July":"Julio","August":"Agosto","September":"Septiembre","October":"Octubre","November":"Noviembre","December":"Diciembre","Show":"Mostrar","Select all rows in this table":"Seleccionar todas las filas de esta tabla","Deselect all rows in this table":"Quitar la selecci\u00f3n a todas las filas de esta tabla","Today":"Hoy","Jan":"Ene","Feb":"Feb","Mar":"Mar","Apr":"Abr","Jun":"Jun","Jul":"Jul","Aug":"Ago","Sep":"Sep","Oct":"Oct","Nov":"Nov","Dec":"Dic","Su":"Do","Mo":"Lu","Tu":"Ma","We":"Mi","Th":"Ju","Fr":"Vi","Sa":"Sa","Not published":"No publicado","Please wait...":"Espere, por favor...","Hide":"Ocultar","mm\/dd\/yy":"mm\/dd\/yy","By @name on @date":"Por @name en @date","By @name":"Por @name","Not in menu":"No est\u00e1 en un men\u00fa","Alias: @alias":"Alias: @alias","No alias":"Sin alias","New revision":"Nueva revisi\u00f3n","Drag to re-order":"Arrastre para reordenar","Changes made in this table will not be saved until the form is submitted.":"Los cambios realizados en esta tabla no se guardar\u00e1n hasta que se env\u00ede el formulario","The changes to these blocks will not be saved until the \u003Cem\u003ESave blocks\u003C\/em\u003E button is clicked.":"Los cambios sobre estos bloques no se guardar\u00e1n hasta que no pulse el bot\u00f3n \u003Cem\u003EGuardar bloques\u003C\/em\u003E.","This permission is inherited from the authenticated user role.":"Este permiso se hereda del rol de usuario registrado.","No revision":"Sin revisi\u00f3n","@number comments per page":"@number comentarios por p\u00e1gina","Requires a title":"Necesita un t\u00edtulo","Not restricted":"Sin restricci\u00f3n","(active tab)":"(solapa activa)","Not customizable":"No personalizable","Restricted to certain pages":"Restringido a algunas p\u00e1ginas","The block cannot be placed in this region.":"El bloque no se puede colocar en esta regi\u00f3n.","Customize dashboard":"Personalizar panel de control","Don\u0027t display post information":"No mostrar informaci\u00f3n del env\u00edo","Re-order rows by numerical weight instead of dragging.":"Reordenar las filas por peso num\u00e9rico en lugar de arrastrar.","Show row weights":"Mostrar pesos de la fila","Hide row weights":"Ocultar pesos de la fila","Autocomplete popup":"Ventana emergente con autocompletado","Searching for matches...":"Buscando coincidencias","Other":"Otro(s)","Remove group":"Eliminar grupo","Apply (all displays)":"Aplicar (todas las presentaciones)","Apply (this display)":"Aplicar (esta presentaci\u00f3n)","Revert to default":"Volver a los valores predeterminados","Available tokens":"Tokens disponibles","Insert this token into your form":"Inserta este comod\u00edn en su formulario","Automatic alias":"Alias autom\u00e1tico"}} };;
+/**
+ * Attaches the calendar behavior to all required fields
+ */
+(function ($) {
+Drupal.behaviors.date_popup = {
+  attach: function (context) {
+  for (var id in Drupal.settings.datePopup) {
+    $('#'+ id).bind('focus', Drupal.settings.datePopup[id], function(e) {
+      if (!$(this).hasClass('date-popup-init')) {
+        var datePopup = e.data;
+        // Explicitely filter the methods we accept.
+        switch (datePopup.func) {
+          case 'datepicker':
+            $(this)
+              .datepicker(datePopup.settings)
+              .addClass('date-popup-init')
+            $(this).click(function(){
+              $(this).focus();
+            });
+            break;
+
+          case 'timeEntry':
+            $(this)
+              .timeEntry(datePopup.settings)
+              .addClass('date-popup-init')
+            $(this).click(function(){
+              $(this).focus();
+            });
+            break;
+          case 'timepicker':
+            // Translate the PHP date format into the style the timepicker uses.
+            datePopup.settings.timeFormat = datePopup.settings.timeFormat
+              // 12-hour, leading zero,
+              .replace('h', 'hh')
+              // 12-hour, no leading zero.
+              .replace('g', 'h')
+              // 24-hour, leading zero.
+              .replace('H', 'HH')
+              // 24-hour, no leading zero.
+              .replace('G', 'H')
+              // AM/PM.
+              .replace('A', 'p')
+              // Minutes with leading zero.
+              .replace('i', 'mm')
+              // Seconds with leading zero.
+              .replace('s', 'ss');
+
+            datePopup.settings.startTime = new Date(datePopup.settings.startTime);
+            $(this)
+              .timepicker(datePopup.settings)
+              .addClass('date-popup-init');
+            $(this).click(function(){
+              $(this).focus();
+            });
+            break;
+        }
+      }
+    });
+  }
+  }
+};
+})(jQuery);
+;
+(function ($) {
+
+/**
+ * Toggle the visibility of a fieldset using smooth animations.
+ */
+Drupal.toggleFieldset = function (fieldset) {
+  var $fieldset = $(fieldset);
+  if ($fieldset.is('.collapsed')) {
+    var $content = $('> .fieldset-wrapper', fieldset).hide();
+    $fieldset
+      .removeClass('collapsed')
+      .trigger({ type: 'collapsed', value: false })
+      .find('> legend span.fieldset-legend-prefix').html(Drupal.t('Hide'));
+    $content.slideDown({
+      duration: 'fast',
+      easing: 'linear',
+      complete: function () {
+        Drupal.collapseScrollIntoView(fieldset);
+        fieldset.animating = false;
+      },
+      step: function () {
+        // Scroll the fieldset into view.
+        Drupal.collapseScrollIntoView(fieldset);
+      }
+    });
+  }
+  else {
+    $fieldset.trigger({ type: 'collapsed', value: true });
+    $('> .fieldset-wrapper', fieldset).slideUp('fast', function () {
+      $fieldset
+        .addClass('collapsed')
+        .find('> legend span.fieldset-legend-prefix').html(Drupal.t('Show'));
+      fieldset.animating = false;
+    });
+  }
+};
+
+/**
+ * Scroll a given fieldset into view as much as possible.
+ */
+Drupal.collapseScrollIntoView = function (node) {
+  var h = document.documentElement.clientHeight || document.body.clientHeight || 0;
+  var offset = document.documentElement.scrollTop || document.body.scrollTop || 0;
+  var posY = $(node).offset().top;
+  var fudge = 55;
+  if (posY + node.offsetHeight + fudge > h + offset) {
+    if (node.offsetHeight > h) {
+      window.scrollTo(0, posY);
+    }
+    else {
+      window.scrollTo(0, posY + node.offsetHeight - h + fudge);
+    }
+  }
+};
+
+Drupal.behaviors.collapse = {
+  attach: function (context, settings) {
+    $('fieldset.collapsible', context).once('collapse', function () {
+      var $fieldset = $(this);
+      // Expand fieldset if there are errors inside, or if it contains an
+      // element that is targeted by the URI fragment identifier.
+      var anchor = location.hash && location.hash != '#' ? ', ' + location.hash : '';
+      if ($fieldset.find('.error' + anchor).length) {
+        $fieldset.removeClass('collapsed');
+      }
+
+      var summary = $('<span class="summary"></span>');
+      $fieldset.
+        bind('summaryUpdated', function () {
+          var text = $.trim($fieldset.drupalGetSummary());
+          summary.html(text ? ' (' + text + ')' : '');
+        })
+        .trigger('summaryUpdated');
+
+      // Turn the legend into a clickable link, but retain span.fieldset-legend
+      // for CSS positioning.
+      var $legend = $('> legend .fieldset-legend', this);
+
+      $('<span class="fieldset-legend-prefix element-invisible"></span>')
+        .append($fieldset.hasClass('collapsed') ? Drupal.t('Show') : Drupal.t('Hide'))
+        .prependTo($legend)
+        .after(' ');
+
+      // .wrapInner() does not retain bound events.
+      var $link = $('<a class="fieldset-title" href="#"></a>')
+        .prepend($legend.contents())
+        .appendTo($legend)
+        .click(function () {
+          var fieldset = $fieldset.get(0);
+          // Don't animate multiple times.
+          if (!fieldset.animating) {
+            fieldset.animating = true;
+            Drupal.toggleFieldset(fieldset);
+          }
+          return false;
+        });
+
+      $legend.append(summary);
+    });
+  }
+};
+
+})(jQuery);
+;
+(function ($) {
+  Drupal.behaviors.vbo = {
+    attach: function(context) {
+      $('.vbo-views-form', context).each(function() {
+        Drupal.vbo.initTableBehaviors(this);
+        Drupal.vbo.initGenericBehaviors(this);
+      });
+    }
+  }
+
+  Drupal.vbo = Drupal.vbo || {};
+  Drupal.vbo.initTableBehaviors = function(form) {
+    // If the table is not grouped, "Select all on this page / all pages"
+    // markup gets inserted below the table header.
+    var selectAllMarkup = $('.vbo-table-select-all-markup', form);
+    if (selectAllMarkup.length) {
+      $('.views-table > tbody', form).prepend('<tr class="views-table-row-select-all even">></tr>');
+      var colspan = $('table th', form).length;
+      $('.views-table-row-select-all', form).html('<td colspan="' + colspan + '">' + selectAllMarkup.html() + '</td>');
+
+      $('.vbo-table-select-all-pages', form).click(function() {
+        Drupal.vbo.tableSelectAllPages(form);
+        return false;
+      });
+      $('.vbo-table-select-this-page', form).click(function() {
+        Drupal.vbo.tableSelectThisPage(form);
+        return false;
+      });
+    }
+
+    $('.vbo-table-select-all', form).show();
+    // This is the "select all" checkbox in (each) table header.
+    $('.vbo-table-select-all', form).click(function() {
+      var table = $(this).closest('table')[0];
+      $('input[id^="edit-views-bulk-operations"]:not(:disabled)', table).attr('checked', this.checked);
+
+      // Toggle the visibility of the "select all" row (if any).
+      if (this.checked) {
+        $('.views-table-row-select-all', table).show();
+      }
+      else {
+        $('.views-table-row-select-all', table).hide();
+        // Disable "select all across pages".
+        Drupal.vbo.tableSelectThisPage(form);
+      }
+    });
+
+    // Set up the ability to click anywhere on the row to select it.
+    $('.views-table tbody tr', form).click(function(event) {
+      if (event.target.tagName.toLowerCase() != 'input' && event.target.tagName.toLowerCase() != 'a') {
+        $('input[id^="edit-views-bulk-operations"]:not(:disabled)', this).each(function() {
+          var checked = this.checked;
+          // trigger() toggles the checkmark *after* the event is set,
+          // whereas manually clicking the checkbox toggles it *beforehand*.
+          // that's why we manually set the checkmark first, then trigger the
+          // event (so that listeners get notified), then re-set the checkmark
+          // which the trigger will have toggled. yuck!
+          this.checked = !checked;
+          $(this).trigger('click');
+          this.checked = !checked;
+        });
+      }
+    });
+  }
+
+  Drupal.vbo.tableSelectAllPages = function(form) {
+    $('.vbo-table-this-page', form).hide();
+    $('.vbo-table-all-pages', form).show();
+    // Modify the value of the hidden form field.
+    $('.select-all-rows', form).val('1');
+  }
+  Drupal.vbo.tableSelectThisPage = function(form) {
+    $('.vbo-table-all-pages', form).hide();
+    $('.vbo-table-this-page', form).show();
+    // Modify the value of the hidden form field.
+    $('.select-all-rows', form).val('0');
+  }
+
+  Drupal.vbo.initGenericBehaviors = function(form) {
+    // Show the "select all" fieldset.
+    $('.vbo-select-all-markup', form).show();
+
+    $('.vbo-select-this-page', form).click(function() {
+      $('input[id^="edit-views-bulk-operations"]', form).attr('checked', this.checked);
+      $('.vbo-select-all-pages', form).attr('checked', false);
+
+      // Toggle the "select all" checkbox in grouped tables (if any).
+      $('.vbo-table-select-all', form).attr('checked', this.checked);
+    });
+    $('.vbo-select-all-pages', form).click(function() {
+      $('input[id^="edit-views-bulk-operations"]', form).attr('checked', this.checked);
+      $('.vbo-select-this-page', form).attr('checked', false);
+
+      // Toggle the "select all" checkbox in grouped tables (if any).
+      $('.vbo-table-select-all', form).attr('checked', this.checked);
+
+      // Modify the value of the hidden form field.
+      $('.select-all-rows', form).val(this.checked);
+    });
+
+    $('.vbo-select', form).click(function() {
+      // If a checkbox was deselected, uncheck any "select all" checkboxes.
+      if (!this.checked) {
+        $('.vbo-select-this-page', form).attr('checked', false);
+        $('.vbo-select-all-pages', form).attr('checked', false);
+        // Modify the value of the hidden form field.
+        $('.select-all-rows', form).val('0')
+
+        var table = $(this).closest('table')[0];
+        if (table) {
+          // Uncheck the "select all" checkbox in the table header.
+          $('.vbo-table-select-all', table).attr('checked', false);
+
+          // If there's a "select all" row, hide it.
+          if ($('.vbo-table-select-this-page', table).length) {
+            $('.views-table-row-select-all', table).hide();
+            // Disable "select all across pages".
+            Drupal.vbo.tableSelectThisPage(form);
+          }
+        }
+      }
+    });
+  }
+
+})(jQuery);
+;
---
+++ sites/default/files/js/js_ZrdWoFDe-ME7OnhNOLTRvMrkXUOLr6rZbGHvujPKKfo.js
@@ -0,0 +1,225 @@
+Drupal.locale = { 'pluralFormula': function ($n) { return Number(($n!=1)); }, 'strings': {"":{"An AJAX HTTP error occurred.":"Hubo un error HTTP AJAX.","HTTP Result Code: !status":"C\u00f3digo de Resultado HTTP: !status","An AJAX HTTP request terminated abnormally.":"Una solicitud HTTP de AJAX termin\u00f3 de manera anormal.","Debugging information follows.":"A continuaci\u00f3n se detalla la informaci\u00f3n de depuraci\u00f3n.","Path: !uri":"Ruta: !uri","StatusText: !statusText":"StatusText: !statusText","ResponseText: !responseText":"ResponseText: !responseText","ReadyState: !readyState":"ReadyState: !readyState","Next":"Siguiente","Edit":"Editar","Sunday":"Domingo","Monday":"Lunes","Tuesday":"Martes","Wednesday":"Mi\u00e9rcoles","Thursday":"Jueves","Friday":"Viernes","Saturday":"S\u00e1bado","Add":"Agregar","Configure":"Configurar","Done":"Hecho","Prev":"Previo","Mon":"Lun","Tue":"Mar","Wed":"Mi\u00e9","Thu":"Jue","Fri":"Vie","Sat":"S\u00e1b","Sun":"Dom","January":"Enero","February":"Febrero","March":"Marzo","April":"Abril","May":"Mayo","June":"Junio","July":"Julio","August":"Agosto","September":"Septiembre","October":"Octubre","November":"Noviembre","December":"Diciembre","Show":"Mostrar","Select all rows in this table":"Seleccionar todas las filas de esta tabla","Deselect all rows in this table":"Quitar la selecci\u00f3n a todas las filas de esta tabla","Today":"Hoy","Jan":"Ene","Feb":"Feb","Mar":"Mar","Apr":"Abr","Jun":"Jun","Jul":"Jul","Aug":"Ago","Sep":"Sep","Oct":"Oct","Nov":"Nov","Dec":"Dic","Su":"Do","Mo":"Lu","Tu":"Ma","We":"Mi","Th":"Ju","Fr":"Vi","Sa":"Sa","Not published":"No publicado","Please wait...":"Espere, por favor...","Hide":"Ocultar","mm\/dd\/yy":"mm\/dd\/yy","By @name on @date":"Por @name en @date","By @name":"Por @name","Not in menu":"No est\u00e1 en un men\u00fa","Alias: @alias":"Alias: @alias","No alias":"Sin alias","New revision":"Nueva revisi\u00f3n","Drag to re-order":"Arrastre para reordenar","Changes made in this table will not be saved until the form is submitted.":"Los cambios realizados en esta tabla no se guardar\u00e1n hasta que se env\u00ede el formulario","The changes to these blocks will not be saved until the \u003Cem\u003ESave blocks\u003C\/em\u003E button is clicked.":"Los cambios sobre estos bloques no se guardar\u00e1n hasta que no pulse el bot\u00f3n \u003Cem\u003EGuardar bloques\u003C\/em\u003E.","This permission is inherited from the authenticated user role.":"Este permiso se hereda del rol de usuario registrado.","No revision":"Sin revisi\u00f3n","@number comments per page":"@number comentarios por p\u00e1gina","Requires a title":"Necesita un t\u00edtulo","Not restricted":"Sin restricci\u00f3n","(active tab)":"(solapa activa)","Not customizable":"No personalizable","Restricted to certain pages":"Restringido a algunas p\u00e1ginas","The block cannot be placed in this region.":"El bloque no se puede colocar en esta regi\u00f3n.","Customize dashboard":"Personalizar panel de control","Don\u0027t display post information":"No mostrar informaci\u00f3n del env\u00edo","Re-order rows by numerical weight instead of dragging.":"Reordenar las filas por peso num\u00e9rico en lugar de arrastrar.","Show row weights":"Mostrar pesos de la fila","Hide row weights":"Ocultar pesos de la fila","Autocomplete popup":"Ventana emergente con autocompletado","Searching for matches...":"Buscando coincidencias","Other":"Otro(s)","Remove group":"Eliminar grupo","Apply (all displays)":"Aplicar (todas las presentaciones)","Apply (this display)":"Aplicar (esta presentaci\u00f3n)","Revert to default":"Volver a los valores predeterminados","Available tokens":"Tokens disponibles","Insert this token into your form":"Inserta este comod\u00edn en su formulario","Automatic alias":"Alias autom\u00e1tico"}} };;
+/**
+ * @file
+ * Attaches behaviors for the Dashboard module.
+ */
+
+(function ($) {
+
+/**
+ * Implements Drupal.behaviors for the Dashboard module.
+ */
+Drupal.behaviors.dashboard = {
+  attach: function (context, settings) {
+    $('#dashboard', context).once(function () {
+      $(this).prepend('<div class="customize clearfix"><ul class="action-links"><li><a href="#">' + Drupal.t('Customize dashboard') + '</a></li></ul><div class="canvas"></div></div>');
+      $('.customize .action-links a', this).click(Drupal.behaviors.dashboard.enterCustomizeMode);
+    });
+    Drupal.behaviors.dashboard.addPlaceholders();
+    if (Drupal.settings.dashboard.launchCustomize) {
+      Drupal.behaviors.dashboard.enterCustomizeMode();
+    }
+  },
+
+  addPlaceholders: function() {
+    $('#dashboard .dashboard-region .region').each(function () {
+      var empty_text = "";
+      // If the region is empty
+      if ($('.block', this).length == 0) {
+        // Check if we are in customize mode and grab the correct empty text
+        if ($('#dashboard').hasClass('customize-mode')) {
+          empty_text = Drupal.settings.dashboard.emptyRegionTextActive;
+        } else {
+          empty_text = Drupal.settings.dashboard.emptyRegionTextInactive;
+        }
+        // We need a placeholder.
+        if ($('.placeholder', this).length == 0) {
+          $(this).append('<div class="placeholder"></div>');
+        }
+        $('.placeholder', this).html(empty_text);
+      }
+      else {
+        $('.placeholder', this).remove();
+      }
+    });
+  },
+
+  /**
+   * Enters "customize" mode by displaying disabled blocks.
+   */
+  enterCustomizeMode: function () {
+    $('#dashboard').addClass('customize-mode customize-inactive');
+    Drupal.behaviors.dashboard.addPlaceholders();
+    // Hide the customize link
+    $('#dashboard .customize .action-links').hide();
+    // Load up the disabled blocks
+    $('div.customize .canvas').load(Drupal.settings.dashboard.drawer, Drupal.behaviors.dashboard.setupDrawer);
+  },
+
+  /**
+   * Exits "customize" mode by simply forcing a page refresh.
+   */
+  exitCustomizeMode: function () {
+    $('#dashboard').removeClass('customize-mode customize-inactive');
+    Drupal.behaviors.dashboard.addPlaceholders();
+    location.href = Drupal.settings.dashboard.dashboard;
+  },
+
+  /**
+   * Sets up the drag-and-drop behavior and the 'close' button.
+   */
+  setupDrawer: function () {
+    $('div.customize .canvas-content input').click(Drupal.behaviors.dashboard.exitCustomizeMode);
+    $('div.customize .canvas-content').append('<a class="button" href="' + Drupal.settings.dashboard.dashboard + '">' + Drupal.t('Done') + '</a>');
+
+    // Initialize drag-and-drop.
+    var regions = $('#dashboard div.region');
+    regions.sortable({
+      connectWith: regions,
+      cursor: 'move',
+      cursorAt: {top:0},
+      dropOnEmpty: true,
+      items: '> div.block, > div.disabled-block',
+      placeholder: 'block-placeholder clearfix',
+      tolerance: 'pointer',
+      start: Drupal.behaviors.dashboard.start,
+      over: Drupal.behaviors.dashboard.over,
+      sort: Drupal.behaviors.dashboard.sort,
+      update: Drupal.behaviors.dashboard.update
+    });
+  },
+
+  /**
+   * Makes the block appear as a disabled block while dragging.
+   *
+   * This function is called on the jQuery UI Sortable "start" event.
+   *
+   * @param event
+   *  The event that triggered this callback.
+   * @param ui
+   *  An object containing information about the item that is being dragged.
+   */
+  start: function (event, ui) {
+    $('#dashboard').removeClass('customize-inactive');
+    var item = $(ui.item);
+
+    // If the block is already in disabled state, don't do anything.
+    if (!item.hasClass('disabled-block')) {
+      item.css({height: 'auto'});
+    }
+  },
+
+  /**
+   * Adapts block's width to the region it is moved into while dragging.
+   *
+   * This function is called on the jQuery UI Sortable "over" event.
+   *
+   * @param event
+   *  The event that triggered this callback.
+   * @param ui
+   *  An object containing information about the item that is being dragged.
+   */
+  over: function (event, ui) {
+    var item = $(ui.item);
+
+    // If the block is in disabled state, remove width.
+    if ($(this).closest('#disabled-blocks').length) {
+      item.css('width', '');
+    }
+    else {
+      item.css('width', $(this).width());
+    }
+  },
+
+  /**
+   * Adapts a block's position to stay connected with the mouse pointer.
+   *
+   * This function is called on the jQuery UI Sortable "sort" event.
+   *
+   * @param event
+   *  The event that triggered this callback.
+   * @param ui
+   *  An object containing information about the item that is being dragged.
+   */
+  sort: function (event, ui) {
+    var item = $(ui.item);
+
+    if (event.pageX > ui.offset.left + item.width()) {
+      item.css('left', event.pageX);
+    }
+  },
+
+  /**
+   * Sends block order to the server, and expand previously disabled blocks.
+   *
+   * This function is called on the jQuery UI Sortable "update" event.
+   *
+   * @param event
+   *   The event that triggered this callback.
+   * @param ui
+   *   An object containing information about the item that was just dropped.
+   */
+  update: function (event, ui) {
+    $('#dashboard').addClass('customize-inactive');
+    var item = $(ui.item);
+
+    // If the user dragged a disabled block, load the block contents.
+    if (item.hasClass('disabled-block')) {
+      var module, delta, itemClass;
+      itemClass = item.attr('class');
+      // Determine the block module and delta.
+      module = itemClass.match(/\bmodule-(\S+)\b/)[1];
+      delta = itemClass.match(/\bdelta-(\S+)\b/)[1];
+
+      // Load the newly enabled block's content.
+      $.get(Drupal.settings.dashboard.blockContent + '/' + module + '/' + delta, {},
+        function (block) {
+          if (block) {
+            item.html(block);
+          }
+
+          if (item.find('div.content').is(':empty')) {
+            item.find('div.content').html(Drupal.settings.dashboard.emptyBlockText);
+          }
+
+          Drupal.attachBehaviors(item);
+        },
+        'html'
+      );
+      // Remove the "disabled-block" class, so we don't reload its content the
+      // next time it's dragged.
+      item.removeClass("disabled-block");
+    }
+
+    Drupal.behaviors.dashboard.addPlaceholders();
+
+    // Let the server know what the new block order is.
+    $.post(Drupal.settings.dashboard.updatePath, {
+        'form_token': Drupal.settings.dashboard.formToken,
+        'regions': Drupal.behaviors.dashboard.getOrder
+      }
+    );
+  },
+
+  /**
+   * Returns the current order of the blocks in each of the sortable regions.
+   *
+   * @return
+   *   The current order of the blocks, in query string format.
+   */
+  getOrder: function () {
+    var order = [];
+    $('#dashboard div.region').each(function () {
+      var region = $(this).parent().attr('id').replace(/-/g, '_');
+      var blocks = $(this).sortable('toArray');
+      $.each(blocks, function() {
+        order.push(region + '[]=' + this);
+      });
+    });
+    order = order.join('&');
+    return order;
+  }
+};
+
+})(jQuery);
+;
